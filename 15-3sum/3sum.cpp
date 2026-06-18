007139235d0baf04d0e736db334b0eafb5c4994a
Sneha <snehaa7052@gmail.com>
2026-06-18 09:56:35 +0530

    Time: 44 ms (74.98%) | Memory: 29.1 MB (72.66%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,48 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
+      int n=nums.size();
+      int l,r;
+      sort(nums.begin(),nums.end());
+      for(int i=0;i<n-2;i++)
+      {
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        l=i+1,r=n-1;
 
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
+        while(l<r)
+        {
+            int sum=nums[l]+nums[r]+nums[i];
+            if(sum==0)
+            {
+                ans.push_back({nums[l],nums[r],nums[i]});
+                l++;
+                r--;
 
-        int n = nums.size();
-
-        for(int i = 0; i < n - 2; i++) {
-
-            // Skip duplicate first elements
-            if(i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while(left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if(sum == 0) {
-
-                    ans.push_back({nums[i], nums[left], nums[right]});
-
-                    left++;
-                    right--;
-
-                    // Skip duplicates
-                    while(left < right && nums[left] == nums[left - 1])
-                        left++;
-
-                    while(left < right && nums[right] == nums[right + 1])
-                        right--;
-                }
-                else if(sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
+                while(l<r && nums[l]==nums[l-1]) l++;
+                while(l<r && nums[r]==nums[r+1]) r--;
+            }
+            else if(sum>0){
+                r--;
+            }
+            else {
+            l++;
             }
         }
-
-        return ans;
+      }
+      return ans;  
     }
 };
