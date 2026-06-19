a97443c16c2888f5825ba584d542092fc486bb77
Sneha <snehaa7052@gmail.com>
2026-06-19 09:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (47.39%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -7,11 +7,12 @@ public:
         while(left<=right){
             if(nums[left]==val){
             swap(nums[left],nums[right]);
-            right--;
+            right--;  // we dont move left because.."what if after swaping als0 we get same value as val again, we need new element in front so we have to make sure that we move right pointer to get unique element at the front
         }
-        else left++;
-
+        else {
+            left++;//if not equal to val toh aage badho
         }
+        }//while
         return left;
     }
 };
