7c2a8ec939bb3e8745a1e3ad8849d5c5fbe30104
Sneha <snehaa7052@gmail.com>
2026-07-25 01:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.86%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n=nums.size();
-        int left=0;
-        int right=n-1;
-        while(left<=right){
-            if(nums[left]==val){
-            swap(nums[left],nums[right]);
-            right--;  // we dont move left because.."what if after swaping als0 we get same value as val again, we need new element in front so we have to make sure that we move right pointer to get unique element at the front
+        vector <int> ans;
+        
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] != val){
+                ans.push_back(nums[i]);
+            }
         }
-        else {
-            left++;//if not equal to val toh aage badho
+        
+        for (int i=0; i<ans.size(); i++) {
+            nums[i] = ans[i];
         }
-        }//while
-        return left;
+        return ans.size();
     }
 };
