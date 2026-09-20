2395154c474f96f9b8575ecb07753889095dcce9
Sneha <snehaa7052@gmail.com>
2026-09-20 23:08:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (74.90%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> freq(256, 0);
+        int n1 = s.length();
+        int n2 = t.length();
+        if (n1 != n2)
+            return false;
+        for (int i = 0; i < n1; i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
