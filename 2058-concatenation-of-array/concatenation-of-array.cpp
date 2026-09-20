bb932f0f2cfa2f83c1869d74a6841e04bb8936c2
Sneha <snehaa7052@gmail.com>
2026-09-20 22:38:08 +0530

    Time: 6 ms (0.90%) | Memory: 16.8 MB (81.36%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -4,13 +4,11 @@ public:
         int n=nums.size();
         vector<int>ans(2*n);
         for(int i=0;i<2*n;i++){
-            if(i<n){
-                ans[i]=nums[i];
-            }
-            else {
-                ans[i]=nums[i-n];
-            }
+            if(i<n)
+            ans[i]=nums[i];
+            else
+            ans[i]=nums[i-n];
         }
         return ans;
-    }
+        }
 };
